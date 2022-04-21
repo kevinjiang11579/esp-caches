dd46853b454d89f3e1f02cebfb7e3a41e67a7c03
Raghav Balu <rb3508@columbia.edu>
2022-04-21 19:53:06 -0400

    removed test fifo sv file and commented out struct for memory lookup

--- llc/rtl/llc_fifo_packet.sv
+++ llc/rtl/llc_fifo_packet.sv
@@ -1,19 +1,19 @@
 `include "cache_consts.svh"
 `include "cache_types.svh"
 
-//This structure is used for FIFO between input decoder and local memory
+This structure is used for FIFO between input decoder and local memory
 typedef struct packed{
     llc_set_t set;
     llc_set_t set_next;
     llc_tag_t tag_input;
 }decoder_mem_packet;
 
-typedef struct packed{
-    llc_tag_t tag_input;
-    llc_tag_t tags_mem[`LLC_WAYS];
-    llc_state_t states_mem[`LLC_NUM_PORTS];
-    llc_way_t evict_way_mem;
-}fifo_mem_lookup_packet;
+//typedef struct packed{
+//    llc_tag_t tag_input;
+//    llc_tag_t tags_mem[`LLC_WAYS];
+//    llc_state_t states_mem[`LLC_NUM_PORTS];
+//    llc_way_t evict_way_mem;
+//}fifo_mem_lookup_packet;
 
 typedef struct packed{
     logic is_rst_to_resume; 
